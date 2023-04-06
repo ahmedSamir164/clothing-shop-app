c36c106e7fce3b90bfac4f037196e7e969464524
unknown <ahmedsamir16494@gmail.com>
2023-04-06 05:09:21 +0200

    created director container

---
+++ src/component/categories-container/categories-container.component.jsx
@@ -0,0 +1,12 @@
+import Category from "../category/category.component";
+import "./categories.styles.scss";
+const Categories = ({ categories }) => {
+  return (
+    <div className="categories-container">
+      {categories.map((category) => {
+        return <Category key={category.id} category={category} />;
+      })}
+    </div>
+  );
+};
+export default Categories;
