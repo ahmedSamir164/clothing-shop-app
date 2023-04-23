0959a40a470d9f332cb7f8ddeb04167dd2407309
unknown <ahmedsamir16494@gmail.com>
2023-04-23 16:47:08 +0200

    finished cart dropdown component

--- src/component/product-card/product-card.component.jsx
+++ src/component/product-card/product-card.component.jsx
@@ -1,8 +1,12 @@
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
 import Button from "../sign-up-form/Button/Button.component";
 import "./product-card.styles.scss";
 const ProductCard = ({ product }) => {
   const { name, imageUrl, price } = product;
   console.log(product);
+  const { addItemsTocart } = useContext(CartContext);
+  const addProductTocart = () => addItemsTocart(product);
   return (
     <div className="product-card-container">
       <img src={imageUrl} alt={`${name}`} />
@@ -10,7 +14,9 @@ const ProductCard = ({ product }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <Button buttonType="inverted">Add to cart</Button>
+      <Button buttonType="inverted" onClick={addProductTocart}>
+        Add to cart
+      </Button>
     </div>
   );
 };
